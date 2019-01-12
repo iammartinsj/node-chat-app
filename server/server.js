eea1bc7001547a5d564350b2e30796fb01b6ad37
Steve Martin San Jose <stevemartin.2k16@gmail.com>
2019-01-12 19:05:14 +0800

    Gree new User, and alert others

--- server/server.js
+++ server/server.js
@@ -17,8 +17,19 @@ io.on('connection', (socket) => {
 
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome New User',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user join to chat',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
-    console.log('message', message);
     io.emit('newMessage',{
       from: message.from,
       text: message.text,
